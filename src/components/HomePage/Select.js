bcb4c58a9b2537216e410ea94dfee2500076d0d2
ebharathi123 <insideout123eb@gmail.com>
2022-07-31 09:44:02 +0530

    choose

--- src/components/HomePage/Select.js
+++ src/components/HomePage/Select.js
@@ -54,7 +54,7 @@ const Select = () => {
                        <div className="row">
                             <div className="choose">
                                 <div className="a text-center btn btn-primary" style={{padding:4,borderRadius:2}}>
-                                       <a href={`/select/user/${id}`}  style={{fontSize:17,color:'#fff'}}>Select Bus</a>
+                                       <a href={`/select/user/${id}`}  style={{fontSize:17,color:'#ffffff'}}>Select Bus</a>
                                 </div>
                                 <div className="b text-center" style={{padding:4}}>
                                 <a href="#" style={{fontSize:15,color:'blue'}}>Select Seat</a>
