5005bcbdbcccaedcf02d95ab4abdd4456cfb8456
ebharathi <elayabharathi10102002@gmail.com>
2023-10-28 21:35:04 +0530

    last

--- src/components/Bus/CreditCard.js
+++ src/components/Bus/CreditCard.js
@@ -15,6 +15,7 @@ const CreditCard=()=>{
     const [focused,setFocused]=useState("");
     const [expiryMonthSelect,setExpiryMonthSelect]=useState("");
     const [expiryYearSelect,setExpiryYearSelect]=useState("");
+    const [err,setErr]=useState("");
     //for expiry date
     let months=["01","02","03","04","05","06","07","08","09","10","11","12"];
     let year=[23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
@@ -23,25 +24,36 @@ const CreditCard=()=>{
         setExpiry(expiryMonthSelect+expiryYearSelect);
     },[expiryMonthSelect,expiryYearSelect])
     const updateTable=async()=>{
-            setBtn("PLEASE WAIT...")
-                let s=localStorage.getItem('seats')
-                s=JSON.parse(s)
-                let data={
-                  busId:id,
-                  seats:s
-                }
-                  await axios.post('http://localhost:9000/bus/update',data)
-                  .then((response)=>{
-                    console.log("response for payment-->",response.data)
-                    if(response.data.error==false)
-                    {
-                        setSuccess("SEAT BOOKING CONFIRMED. REDIRECTING....")
-                        setTimeout(() => {
-                            Navigate(`/home/3`)
-                        }, 3000);
-                        
-                    }
-                  })
+            if(name==""||number==""||cvc==""||expiry=="")
+            {
+              setErr("Please Fill All the Credentials!!!")
+              setTimeout(() => {
+                 setErr("");
+              }, 5000);
+              return;
+            }
+            else
+            {
+              setBtn("PLEASE WAIT...")
+                  let s=localStorage.getItem('seats')
+                  s=JSON.parse(s)
+                  let data={
+                    busId:id,
+                    seats:s
+                  }
+                    await axios.post('http://localhost:9000/bus/update',data)
+                    .then((response)=>{
+                      console.log("response for payment-->",response.data)
+                      if(response.data.error==false)
+                      {
+                          setSuccess("SEAT BOOKING CONFIRMED. REDIRECTING....")
+                          setTimeout(() => {
+                              Navigate(`/home/3`)
+                          }, 3000);
+                          
+                      }
+                    })
+            }
         
     }
    return(
@@ -105,7 +117,7 @@ const CreditCard=()=>{
                  <br/>
                  <br/>
                  <div className='text-center'>
-                   <a className='' style={{fontSize:14,color:'black',fontWeight:400}}>{success}</a>
+                   <a className='' style={{fontSize:14,color:'red',fontWeight:400}}>{err}</a>
                  </div>
                  <br/>
                  <br/>
