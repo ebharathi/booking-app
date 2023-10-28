b95d3060f4a1d550b8981f427ad2ce9942607ae2
ebharathi <elayabharathi10102002@gmail.com>
2023-10-28 14:23:25 +0530

    removed home files

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import {BrowserRouter as Router,Routes,Route,Navigate,Switch} from 'react-router
 import Start from './components/Start';
 import Login from './components/Login'
 import Signup from './components/Signup';
-
-import Select from './components/HomePage/Select';
-import Seat from './components/HomePage/Seat';
-import Payment from './components/HomePage/Payment';
 const App = () => {
   return (
     <Router>
@@ -15,9 +11,6 @@ const App = () => {
                 <Route exact path="/" element={<Start />}/>
                 <Route exact path="/login" element={<Login />}/>
                 <Route exact path="/signup" element={<Signup />}/>
-                <Route exact path="/select/user/:id" element={<Select />} />
-                <Route exact path="/select/user/:id/seat/bus/:id" element={<Seat />} />
-                <Route exact path="/select/user/:id/seat/bus/:id/payment/count/:id/detail/:id" element={<Payment />} />
           </Routes>
     </Router>
   )
